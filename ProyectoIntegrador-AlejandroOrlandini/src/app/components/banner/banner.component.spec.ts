d66003230cc03d81e5ada318aadb51e18d512377
Portland1417 <alejandroorlandini84@gmail.com>
2022-11-28 09:28:56 -0300

    banner

--- ProyectoIntegrador-AlejandroOrlandini/src/app/components/banner/banner.component.spec.ts
+++ ProyectoIntegrador-AlejandroOrlandini/src/app/components/banner/banner.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SocialComponent } from './social.component';
+import { BannerComponent } from './banner.component';
 
-describe('SocialComponent', () => {
-  let component: SocialComponent;
-  let fixture: ComponentFixture<SocialComponent>;
+describe('BannerComponent', () => {
+  let component: BannerComponent;
+  let fixture: ComponentFixture<BannerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SocialComponent ]
+      declarations: [ BannerComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SocialComponent);
+    fixture = TestBed.createComponent(BannerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
